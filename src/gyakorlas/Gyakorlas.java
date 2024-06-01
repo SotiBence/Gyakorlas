063e0d18db950bd1ad749daadcf07cbf3834f896
Bence <Soti2200@gmail.com>
2024-06-01 09:18:21 +0200

    osszegzes tetele

--- src/gyakorlas/Gyakorlas.java
+++ src/gyakorlas/Gyakorlas.java
@@ -14,8 +14,16 @@ public class Gyakorlas {
     }
     
     private static int osszegzes(){
-        return 0;
+        int[] tomb = {3, 8, 2, 4};
+        int osszeg = 0;
+        for (int i=0; i<tomb.length; i++) {
+            osszeg = osszeg + tomb[i];
+        }    
+		System.out.println(osszeg);
+        return osszeg;
     }
+    
+    
 
     private static void konzolraIr(String kimenet) {
         System.out.print(kimenet);
