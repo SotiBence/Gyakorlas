28e8683fe14e4343bf06938f98cc9f5edf499c7f
Bence <Soti2200@gmail.com>
2024-06-01 10:18:16 +0200

    osszes tetel kesz

--- src/gyakorlas/Gyakorlas.java
+++ src/gyakorlas/Gyakorlas.java
@@ -3,7 +3,8 @@ package gyakorlas;
 
 
 public class Gyakorlas {
-
+    
+    private static int[] sorozat = {5, 6, 2, 4, 1};
 
     public static void main(String[] args) {
         feladatok();
@@ -14,21 +15,40 @@ public class Gyakorlas {
     }
     
     private static int osszegzes(){
-        int[] tomb = {3, 8, 2, 4};
         int osszeg = 0;
-        for (int i=0; i<tomb.length; i++) {
-            osszeg = osszeg + tomb[i];
-        }    
+        for (int i = 0; i < sorozat.length; i++) {
+            osszeg = osszeg + sorozat[i];
+        }
 		System.out.println(osszeg);
         return osszeg;
     }
     
     private static int eldontes(){
+        int n = sorozat.length;
+        int ker = 2;
+        
+        int i = 0;
+        while(i < n && sorozat[i] != ker){
+        i++;
+        if(i < n){
+            System.out.println("Van ilyen szám");
+        }else{
+            System.out.println("Nincs ilyen szám");
+        }
+    }
         return 0;
     }
     
     private static int megszamlalas(){
-        return 0;
+        int n = sorozat.length;
+        int szamlalo = 0;
+        for (int i = 0; i < n; i++) {
+            if (sorozat[i] > 5) {
+                szamlalo++;
+            }
+            System.out.println(szamlalo);
+        }
+        return szamlalo;
     }
     
     private static int minKivalasztas(){
@@ -42,6 +62,14 @@ public class Gyakorlas {
     }
     
     private static int kivalasztas(){
+        int n = sorozat.length;
+        int ker =2;
+        
+        int i =0;
+        while(sorozat[i] != ker){
+            i++;
+        }
+        System.out.printf("%d\n", i + 1);
         
         return 0;
     }
