a6bb27101f40f8e53fc37222eb9fd8b399e687e1
Bence <Soti2200@gmail.com>
2024-06-01 09:41:55 +0200

    osszes tetel

--- src/gyakorlas/Gyakorlas.java
+++ src/gyakorlas/Gyakorlas.java
@@ -23,7 +23,43 @@ public class Gyakorlas {
         return osszeg;
     }
     
+    private static int eldontes(){
+        return 0;
+    }
     
+    private static int megszamlalas(){
+        return 0;
+    }
+    
+    private static int minKivalasztas(){
+        
+    return 0;
+    }
+    
+    private static int maxKivalasztas(){
+        
+        return 0;
+    }
+    
+    private static int kivalasztas(){
+        
+        return 0;
+    }
+    
+    private static boolean eldontesEgy(){
+       
+        return false;
+    }
+    
+    private static boolean eldontesMind(){
+        
+        return false;
+    }
+    
+    private static int linKereses(){
+        
+        return -1;
+    }
 
     private static void konzolraIr(String kimenet) {
         System.out.print(kimenet);
